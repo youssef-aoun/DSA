882579255c125e988e0e133589b66988e9ca9ab6
YoussefAoun <night_mare_y12@hotmail.com>
2024-01-19 02:02:58 +0200

    Fixing the Queue class

---
+++ src/datastructures/queue/Main.java
@@ -0,0 +1,27 @@
+package datastructures.queue;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        Queue myQueue = new Queue(2);
+        myQueue.enqueue(1);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myQueue.dequeue().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myQueue.dequeue().value);
+        // (0) Items - Returns null
+        System.out.println(myQueue.dequeue());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
+    }
+}
---
+++ src/datastructures/queue/Queue.java
@@ -0,0 +1,72 @@
+package datastructures.queue;
+
+public class Queue {
+
+    Node front, back;
+    int length;
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        front = newNode;
+        back = newNode;
+        length = 1;
+    }
+
+    public void enqueue(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            front = newNode;
+            back = newNode;
+            length = 1;
+            return;
+        }
+        back.next = newNode;
+        back = newNode;
+        length++;
+    }
+
+    public Node dequeue(){
+        if(length == 0)
+            return null;
+        Node temp = front;
+        if(length == 1){
+            front = null;
+            back = null;
+            length=0;
+            return temp;
+        }
+        front = front.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+    public void getFirst() {
+        if (front == null) {
+            System.out.println("First: null");
+        } else {
+            System.out.println("First: " + front.value);
+        }
+    }
+
+    public void getLast() {
+        if (back == null) {
+            System.out.println("Last: null");
+        } else {
+            System.out.println("Last: " + back.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    class Node{
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
