19d558cd7c0c307306f6901a8c7077bc7ce638dd
YoussefAoun <y.aoun@outlook.com>
2024-01-19 02:08:40 +0200

    Adding the Factorial.java class

---
+++ src/Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    public static void main(String[] args){
+        System.out.println(factorial(5));
+    }
+    public static int factorial(int number){
+        if(number == 1){
+            return 1;
+        }
+        return number*factorial(number-1);
+    }
+}
